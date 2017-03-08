2abacc74e3e13e0f1e7a8bc167d3f826faa3b5e8
Elias Leyton <elias.leyton@rateinc.cl>
2017-03-07 23:59:08 -0300

    update data json

--- dev/app.js
+++ dev/app.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:2jXvp67ztecHEJjgaj55Rd",
                     download_url: "http://smarturl.it/run-run",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/NtoQMqzGrl4"
                 }, {
                     artist: "Siempre Llueve al Atardecer",
                     name: "Siempre Llueve al Atardecer",
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:4mfqZ1kWnGE4lGv9ZGHoLt",
                     download_url: "http://smarturl.it/siempre-llueve-slaa",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/vsauNrtx-oc"
                 }, {
                     artist: "Emisario Greda",
                     name: "Gotas de Cristal",
@@ -76,7 +76,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:1njO74Zrn3fy3bAKP0Qf4P",
                     download_url: "http://smarturl.it/las-olas-amigxs",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/VhzaL_nWRP0"
                 }, {
                     artist: "Patio Solar",
                     name: "Los Movimientos",
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:1s8PZrlLjUkD0YJSI3FXES",
                     download_url: "http://smarturl.it/los-movimientos",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/OWJhuN09JbY"
                 }, {
                     artist: "Ablusion",
                     name: "Soluciones Temporales",
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:0stmV65817fd22I0FaVLYj",
                     download_url: "http://smarturl.it/ablusion-st",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/gnIGyFjWqtU"
                 }, {
                     artist: "Los Valentina",
                     name: "Señoras",
@@ -103,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:0JtXn99bSPKUp8q6Dyn3uH",
                     download_url: "http://smarturl.it/senoras",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/JOLh1DEaVqc"
                 }, 
                 {
                     artist: "MonoAzul",
@@ -113,7 +113,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:32NX2nUKE5fgAsHFDOlaCU",
                     download_url: "http://smarturl.it/monoazul-regalo",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/zW4cregBE1o"
                 }
             ] 
         },
@@ -128,7 +128,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:1Mxq0Z921p5nhXwvmxli1a",
                     download_url: "http://smarturl.it/silabario-las-luces",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/pOYMEFwUtA0"
                 }, {
                     artist: "Centinela Mira al Cielo",
                     name: "Mañana al Vuelo",
@@ -137,7 +137,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:7GCiXLLsGzpCOeH3WRjMOK",
                     download_url: "http://smarturl.it/centinela-al-vuelo",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/_VIKgcbeoFQ"
                 }, {
                     artist: "Varios Artistas",
                     name: "1er Compilado Piloto",
@@ -146,7 +146,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:1dVyZSm2KvcXOqM4bFrDdv",
                     download_url: "http://smarturl.it/1er-compilado-piloto",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/_VIKgcbeoFQ"
                 }, {
                     artist: "Niños del Cerro",
                     name: "Nonato Coo",
@@ -164,16 +164,16 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:48cDDnj9zf1VXk0OE4WJRK",
                     download_url: "http://smarturl.it/jose-de-los-rayos",
                     description: "",
-                    youtube_url: false
+                    youtube_url: ""
                 },{
                     artist: "Silabario",
                     name: "Portugal",
                     slug: "",
                     type: "Simple",
                     spotify_uri: "spotify:album:1wEYJsFRu4ufir7Da7JzPF",
-                    download_url: "#",
+                    download_url: "",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/CjMNQUw2Tzo"
                 }, 
                 {
                     artist: "Pujem",
@@ -193,11 +193,10 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:2olM5q3eW5YEkcpz5D4nkU",
                     download_url: "http://smarturl.it/temporada",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/Y5v_lziIzo0"
                 }
             ]
-        },
-        {
+        },{
             year : "2014",
             releases: [
                 {
@@ -208,7 +207,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     spotify_uri: "spotify:album:07UZIdppT7rf6Xh9Xfp7Dl",
                     download_url: "http://smarturl.it/fiesta-mala",
                     description: "",
-                    youtube_url: false
+                    youtube_url: "https://youtu.be/z5XC_8qud6I"
                 }, {
                     artist: "Pujem",
                     name: "Miedo a la lluvia",
